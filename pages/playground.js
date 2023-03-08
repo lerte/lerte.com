534dd0d255c949e41ea33010571fe1953ef8729b
lerte smith <lerte@qq.com>
2023-03-08 19:42:24 +0800

    update

--- pages/playground.js
+++ pages/playground.js
@@ -1,14 +1,31 @@
-import siteMetadata from '@/data/siteMetadata'
 import { PageSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
+import { WebContainer } from '@webcontainer/api'
 
-export default function Projects() {
+export function getStaticProps() {
+  let webcontainerInstance = null
+  if (typeof window !== 'undefined') {
+    window.addEventListener('load', async () => {
+      webcontainerInstance = await WebContainer.boot()
+    })
+  }
+  return {
+    props: {
+      webcontainerInstance,
+    },
+  }
+}
+
+export default function Playground(props) {
   return (
     <>
       <PageSEO
         title={`Playground - ${siteMetadata.author}`}
         description={siteMetadata.description}
       />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">todo</div>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        {props.webcontainerInstance}
+      </div>
     </>
   )
 }
