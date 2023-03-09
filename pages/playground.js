ea289de4835b655331edaa73d555058cbf1aa25a
lerte smith <lerte@qq.com>
2023-03-09 13:12:56 +0800

    webcontainer

--- pages/playground.js
+++ pages/playground.js
@@ -1,31 +1,36 @@
 import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
 import { WebContainer } from '@webcontainer/api'
+import { useEffect, useState } from 'react'
 
-export function getStaticProps() {
-  let webcontainerInstance = null
-  if (typeof window !== 'undefined') {
-    window.addEventListener('load', async () => {
-      webcontainerInstance = await WebContainer.boot()
-    })
-  }
-  return {
-    props: {
-      webcontainerInstance,
-    },
-  }
-}
+export default function Playground() {
+  const [output, setOutput] = useState('')
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('load', async () => {
+        setOutput('booting...')
+        const wc = await WebContainer.boot()
+        setOutput('booting complete')
 
-export default function Playground(props) {
+        const process = await wc.spawn('node', ['-v'])
+        process.output.pipeTo(
+          new WritableStream({
+            write: (chunk) => setOutput(`Current node version: ${chunk}`),
+          })
+        )
+        if (await process.exit) {
+          setOutput(`Process failed and exited with code ${process?.exit}`)
+        }
+      })
+    }
+  }, [])
   return (
     <>
       <PageSEO
         title={`Playground - ${siteMetadata.author}`}
         description={siteMetadata.description}
       />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        {props.webcontainerInstance}
-      </div>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">{output}</div>
     </>
   )
 }
