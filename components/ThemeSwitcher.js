8d490020edc1c7614227df4b1f28e305fb105832
lerte smith <lerte@qq.com>
2023-03-28 02:35:40 +0800

    prettier

--- components/ThemeSwitcher.js
+++ components/ThemeSwitcher.js
@@ -1,12 +1,24 @@
 import { useTheme } from 'next-themes'
+import { useState, useEffect } from 'react'
 
 export default function () {
+  const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
   return (
-    <div>
-      The current theme is : {theme}
-      <button onClick={() => setTheme('light')}>Light Mode</button>
-      <button onClick={() => setTheme('dark')}>Dark Mode</button>
-    </div>
+    <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+      <option value="system">System</option>
+      <option value="dark">Dark</option>
+      <option value="light">Light</option>
+    </select>
   )
 }
