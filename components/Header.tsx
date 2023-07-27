6b1d880e2dd78cce68e14b70924eeb3411758448
lerte smith <lerte@qq.com>
2023-07-28 00:29:03 +0800

    header adjust dark mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import ThemeSwitch from './ThemeSwitch'
 
 export default function Header() {
   return (
-    <header className="px-4 h-16 shadow-md backdrop-blur sticky top-0 z-10">
+    <header className="px-4 h-16 shadow-md backdrop-blur sticky top-0 z-10 dark:bg-white/10">
       <nav className="h-full mx-auto container flex justify-between items-center">
         <Link href="/">
           <span className="text-4xl">👨‍💻</span>
