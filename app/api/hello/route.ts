57f4ae20eae9f05bde88716b0fbc45e98a326e90
lerte smith <lerte@qq.com>
2023-07-29 22:48:42 +0800

    add api route

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -3,9 +3,8 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   try {
-    const result =
-      await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`
-    return NextResponse.json({ result }, { status: 200 })
+    const result = await sql`SELECT 1+1;`
+    return NextResponse.json({ ...result }, { status: 200 })
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 })
   }
