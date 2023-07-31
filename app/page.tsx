e5935fb205f7b43891e8884c4f5483781a3231cd
lerte smith <lerte@qq.com>
2023-07-31 11:18:49 +0800

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,12 @@
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col justify-between min-h-screen">
       <Header />
-      <main className="flex flex-col min-h-[calc(100vh-64px)] gap-4">
-        <div className="grid grid-cols-3 gap-4 max-w-7xl mx-auto">
-          <img
-            alt="Tailwind CSS"
-            className="object-cover w-80"
-            src="https://cdn.jsdelivr.net/npm/devicon@2.15.1/icons/tailwindcss/tailwindcss-plain.svg"
-          />
-          <img
-            alt="Vue"
-            className="object-cover w-80"
-            src="https://cdn.jsdelivr.net/npm/devicon@2.15.1/icons/vuejs/vuejs-original.svg"
-          />
-          <img
-            alt="Vuetify"
-            className="object-cover w-80"
-            src="https://cdn.jsdelivr.net/npm/devicon@2.15.1/icons/vuetify/vuetify-original.svg"
-          />
-          <img
-            alt="Nuxt"
-            className="object-cover w-80"
-            src="https://cdn.jsdelivr.net/npm/devicon@2.15.1/icons/nuxtjs/nuxtjs-original.svg"
-          />
-          <img
-            alt="React"
-            className="object-cover w-80"
-            src="https://cdn.jsdelivr.net/npm/devicon@2.15.1/icons/react/react-original.svg"
-          />
-          <img
-            alt="React"
-            className="object-cover w-80"
-            src="https://cdn.jsdelivr.net/npm/devicon@2.15.1/icons/nextjs/nextjs-original-wordmark.svg"
-          />
-        </div>
-        <h1 className="mx-auto animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-6xl font-black text-transparent">
-          关注抖音 @lerte
-        </h1>
-      </main>
-    </>
+      <main className="flex-grow"></main>
+      <Footer />
+    </div>
   )
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-between">
+        <p>Lerte &copy;2023 All Rights Reserved</p>
+        <p className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text font-extrabold text-transparent">
+          关注抖音 @lerte
+        </p>
+      </div>
+    </div>
+  )
+}
