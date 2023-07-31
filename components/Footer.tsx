0f8fcb75e4f6b3b0078a714a5a49ea643908073c
lerte smith <lerte@qq.com>
2023-07-31 11:29:02 +0800

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
-    <div className="container mx-auto">
-      <div className="flex justify-between">
+    <footer className="py-2 dark:bg-white/10 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-2px_rgba(0,0,0,0.1)]">
+      <div className="flex justify-between container mx-auto">
         <p>Lerte &copy;2023 All Rights Reserved</p>
         <p className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text font-extrabold text-transparent">
           关注抖音 @lerte
         </p>
       </div>
-    </div>
+    </footer>
   )
 }
