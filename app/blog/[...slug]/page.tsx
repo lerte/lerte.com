ffc4395b92ad0be669c0582dcaacc4d587c53cea
lerte smith <lerte@qq.com>
2023-11-01 20:21:50 +0800

    动态路由

---
+++ app/blog/[...slug]/page.tsx
@@ -0,0 +1,3 @@
+export default function Page({ params }) {
+  return <div className="text-6xl">blog dynamic page {params.slug}</div>
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className="text-6xl">Blog List Page</div>
+}
