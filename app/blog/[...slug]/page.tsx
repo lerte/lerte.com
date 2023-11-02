2d0f385923f84c7603ae2ae183d102ba8c35f626
lerte smith <lerte@qq.com>
2023-11-02 15:21:14 +0800

    remove blog page

--- app/blog/[...slug]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page({ params }) {
-  return <div className="text-6xl">blog dynamic page {params.slug}</div>
-}
--- app/blog/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <div className="text-6xl">Blog List Page</div>
-}
