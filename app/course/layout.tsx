5a7b47d715798e297c6c0fadbb5c13469065fadc
lerte smith <lerte@qq.com>
2023-11-09 23:00:54 +0800

    fix eslint

--- app/course/layout.tsx
+++ app/course/layout.tsx
@@ -5,11 +5,11 @@ import Footer from '@/components/Footer'
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col justify-between min-h-screen">
-      {/* <Header /> */}
+      <Header />
       <main className="grow p-2 bg-slate-900 grid place-content-center">
         {children}
       </main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   )
 }
