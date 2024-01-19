e48a7ed6e8a50c2f8effd512306048eda5119ede
lerte smith <lerte@qq.com>
2024-01-19 17:13:41 +0800

    update

---
+++ app/api/[slug]/route.ts
@@ -0,0 +1,9 @@
+import { type NextRequest } from 'next/server'
+
+export function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const query = searchParams.get('query')
+  // query is "hello" for /api/search?query=hello
+
+  return new Response('Hello, world!' + query, { status: 200 })
+}
