14c29a47a9e2abce98e0c706a49520e3a617641b
lerte smith <lerte@qq.com>
2024-07-26 05:55:25 +0800

    update footer year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="px-4 py-2 dark:bg-white/10 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-2px_rgba(0,0,0,0.1)]">
       <div className="flex justify-between container mx-auto">
-        <p>Lerte &copy;2023 All Rights Reserved</p>
+        <p>Lerte &copy;{new Date().getFullYear()} All Rights Reserved</p>
         <p className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text font-extrabold text-transparent">
           关注抖音 @lerte
         </p>
