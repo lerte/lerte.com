e54d08351ad17bc8e76148139f8c884b20a697b1
lerte smith <lerte@qq.com>
2024-08-06 01:24:06 +0800

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -15,10 +15,10 @@ function Main() {
   return (
     <main className="flex flex-col max-w-7xl mx-auto px-4">
       <h1 className="text-6xl pb-8">Projects</h1>
-      <div className="space-y-4 columns-1 md:columns-3 text-white">
+      <div className="space-y-4 pb-4 columns-1 sm:columns-2 text-white">
         <div className="relative group border rounded-lg shadow-md overflow-hidden">
           <h2 className="p-2 text-lg w-full bg-amber-800 absolute z-10 transition -translate-y-full group-hover:translate-y-0">
-            React Material Design Components Library
+            Actify - React Material Design Components Library
           </h2>
           <a target="_blank" href="https://actifyjs.com">
             <img
@@ -30,7 +30,7 @@ function Main() {
         </div>
         <div className="relative group border rounded-lg shadow-md overflow-hidden">
           <h2 className="p-2 text-lg w-full bg-[#151429] absolute z-10 transition -translate-y-full group-hover:translate-y-0">
-            The best ngrok gui client.
+            Ngroker - the best ngrok gui client.
           </h2>
           <a target="_blank" href="https://ngroker.com">
             <img
@@ -45,10 +45,22 @@ function Main() {
             Excalidraw gui client.
           </h2>
           <img
-            alt="actify"
+            alt="excalidraw"
             src="/images/excalidraw.png"
             className="brightness-75 hover:brightness-100 transition-all"
           />
+        </div>
+        <div className="relative group border rounded-lg shadow-md overflow-hidden">
+          <h2 className="p-2 text-lg w-full bg-red-800 absolute z-10 transition -translate-y-full group-hover:translate-y-0">
+            Taildoor - the essential tool for Tailwind CSS
+          </h2>
+          <a target="_blank" href="https://www.taildoor.com">
+            <img
+              alt="taildoor"
+              src="/images/taildoor.png"
+              className="brightness-75 hover:brightness-100 transition-all"
+            />
+          </a>
         </div>
       </div>
     </main>
